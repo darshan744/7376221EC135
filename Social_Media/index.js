dfee228e761946d40f0062f164a0aac59402560a
darshan744 <darshankumaravelk@gmail.com>
2025-03-20 11:24:28 +0530

    Finished comments posts users url

--- Social_Media/index.js
+++ Social_Media/index.js
@@ -5,12 +5,14 @@ dotenv.config();
 const API_URL = 'http://20.244.56.144/test'
 const port = process.env.PORT;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+const tokenStrig = `Bearer ${ACCESS_TOKEN}`
 const app = express();
 app.use(express.json())
 app.use((req , res , next)=>{
     console.log(`${req.method} - TO - ${req.url}`)
     next();
 })
+
 app.get('/users' , async (req , res)=>{
     console.log(`Bearer ${ACCESS_TOKEN}`);
     try {
@@ -19,7 +21,7 @@ app.get('/users' , async (req , res)=>{
         const response = await axios.get(url , {
             headers:{
                 "Content-Type":"application/json",
-                Authorization:`Bearer ${ACCESS_TOKEN}`,   
+                Authorization:tokenStrig,   
             }
         });
         console.log(response.status);
@@ -36,7 +38,7 @@ app.get('/users/:id/posts' , async (req , res)=> {
     try {
         const response = await axios.get(url , {
             headers:{
-                Authorization : `Bearer ${ACCESS_TOKEN}`
+                Authorization : tokenStrig
             }
         })
         res.json(response.data);
@@ -45,6 +47,21 @@ app.get('/users/:id/posts' , async (req , res)=> {
         res.status(500).send('errror in server');
     }
     
+})
+
+app.get("/posts/:id/comments" , async (req , res)=>{
+    try{
+        const id = req.params.id;
+        const url = `${API_URL}/posts/${id}/comments`;
+        const response = await axios.get(url , {headers:{
+            Authorization:tokenStrig
+        }})
+        res.json(response.data)
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).send('error in server')
+    }
 })
 
 app.get('/check' , (req , res)=>res.send('hi'))
