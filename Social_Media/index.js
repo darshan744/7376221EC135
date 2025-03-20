af538c1ecc024c06a4a28829d9c5a073cbbc538b
darshan744 <darshankumaravelk@gmail.com>
2025-03-20 11:45:16 +0530

    wrote function for token

--- Social_Media/index.js
+++ Social_Media/index.js
@@ -15,13 +15,13 @@ app.use((req , res , next)=>{
 
 app.get('/users' , async (req , res)=>{
     console.log(`Bearer ${ACCESS_TOKEN}`);
+    const token = await getToken()
     try {
         const url = `${API_URL}/users`;
         console.log(url);
         const response = await axios.get(url , {
             headers:{
-                "Content-Type":"application/json",
-                Authorization:tokenStrig,   
+                Authorization:token,   
             }
         });
         console.log(response.status);
@@ -34,11 +34,12 @@ app.get('/users' , async (req , res)=>{
 
 app.get('/users/:id/posts' , async (req , res)=> {
     const id = req.params.id;
+    const token = await getToken()
     const url = `${API_URL}/users/${id}/posts`;
     try {
         const response = await axios.get(url , {
             headers:{
-                Authorization : tokenStrig
+                Authorization : token
             }
         })
         res.json(response.data);
@@ -53,8 +54,10 @@ app.get("/posts/:id/comments" , async (req , res)=>{
     try{
         const id = req.params.id;
         const url = `${API_URL}/posts/${id}/comments`;
+        const token = await getToken()
+
         const response = await axios.get(url , {headers:{
-            Authorization:tokenStrig
+            Authorization:token
         }})
         res.json(response.data)
     }
@@ -66,3 +69,17 @@ app.get("/posts/:id/comments" , async (req , res)=>{
 
 app.get('/check' , (req , res)=>res.send('hi'))
 app.listen(port , ()=>console.log(`Listening in ${port}`));
+
+async function getToken() {
+    const object = {
+            companyName: process.env.companyNmae,
+            clientID: process.env.clientID,
+            clientSecret: process.env.clientSecret,
+            ownerName:process.env.ownerName,
+            ownerEmail:process.env.ownerEmail,
+            rollNo:process.env.rollNo
+        }
+        const authURL = process.env.AUTH_URL;
+        const response = await axios.post("http://20.244.56.144/test/auth" , object)
+        return response.data.access_token;
+}
